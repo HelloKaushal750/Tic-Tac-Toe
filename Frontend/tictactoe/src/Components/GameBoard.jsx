36f1d7cb5615a6e5fcd3ad9eca23256083999295
HelloKaushal750 <kaushalv079@gmail.com>
2023-08-25 00:47:23 +0530

    Frontent CSS part

--- Frontend/tictactoe/src/Components/GameBoard.jsx
+++ Frontend/tictactoe/src/Components/GameBoard.jsx
@@ -1,12 +1,17 @@
 import { useEffect, useState } from "react";
 import "../Styles/GameBoard.css";
+import Confetti from "react-confetti";
+import axios from "axios";
 
 function GameBoard() {
   const [board, setBoard] = useState(Array(9).fill(""));
   const [currentPlayer, setCurrentPlayer] = useState("X");
   const [winner, setWinner] = useState("");
+  const [boolean, setBoolean] = useState(true);
+  // const { width, height } = useWindowSize();
 
   useEffect(() => {
+    console.log(boolean);
     checkWinner();
   }, [board]);
 
@@ -33,22 +38,30 @@ function GameBoard() {
 
     for (const combination of winningCombinations) {
       const [a, b, c] = combination;
-      if (board[a] !== "/" && board[a] === board[b] && board[a] === board[c]) {
+      if (board[a] !== "" && board[a] === board[b] && board[a] === board[c]) {
+        axios
+          .post("http://localhost:7000/games", { data: board, winner })
+          .then((res) => console.log(res))
+          .catch((err) => console.log(err));
         setWinner(board[a]);
+        setBoolean(false);
         return;
       }
     }
 
     if (!board.includes("")) {
       setWinner("DRAW");
+      setBoolean(false);
     }
   };
 
   const renderWinnerMessage = () => {
-    if (winner === "X" || winner === "O") {
-      return <div data-testid="winner">{`${winner} Is Winner`}</div>;
+    if (winner === "X") {
+      return <div className="winner">{`Player 1 is Winner`}</div>;
+    } else if (winner === "O") {
+      return <div className="winner">{`Player 2 is Winner`}</div>;
     } else if (winner === "DRAW") {
-      return <div data-testid="winner">Draw</div>;
+      return <div className="draw">It's a Draw</div>;
     } else {
       return null;
     }
@@ -58,13 +71,39 @@ function GameBoard() {
     setBoard(Array(9).fill(""));
     setCurrentPlayer("X");
     setWinner(null);
+    setBoolean(true);
   };
 
   return (
     <div className="tictactoegame">
+      {(winner === "X" || winner === "O") && <Confetti recycle={false} />}
       <div class="fade-in-heading">
         <h1 className="heading">Welcome To Tic-Tac-Toe</h1>
       </div>
+      {boolean ? (
+        <div
+          style={{
+            height: "50px",
+            marginTop: "-40px",
+            color: "white",
+            display: "flex",
+            gap: "80px",
+            justifyContent: "center",
+            animation: "fadeIn 5s",
+          }}
+        >
+          <h1>
+            Player 1 : <span style={{ color: "#0d9bea" }}>X</span>
+          </h1>
+          <h1>
+            Player 2 : <span style={{ color: "#fe0202" }}>O</span>
+          </h1>
+        </div>
+      ) : (
+        <div style={{ height: "50px", marginTop: "-40px" }}>
+          {renderWinnerMessage()}
+        </div>
+      )}
       <div className="game_board">
         {board?.map((value, index) => {
           return (
@@ -79,10 +118,20 @@ function GameBoard() {
           );
         })}
       </div>
-      <div className="winnername">{renderWinnerMessage()}</div>
-      <div>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(2,1fr)",
+          gap: "20px",
+          margin: "auto",
+          width: "30%",
+        }}
+      >
         <button onClick={handleNewGame} className="newgame">
           New Game
+        </button>
+        <button onClick={handleNewGame} className="newgame">
+          Previous Games
         </button>
       </div>
     </div>
